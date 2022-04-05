9f0eabf1c60ae200c147caf0cbfdd640e29f7f02
Sebastian Toledano <sebastoledano@outlook.com>
2022-04-05 13:07:59 -0300

    javascript.

---
+++ algoritmo1-1.js
@@ -0,0 +1,13 @@
+
+document.write("<div align=center><h1>Mis respetos a los de Sombrero <FONT COLOR='grey'>Gris</FONT></h1></div>");
+document.write("<div align=center><img src='../imagenes/pin.gif' alt='gif'</div>");
+/*
+Los ../ se usan para navegar en sub directorios dentro del directorio
+principal.
+Si solo escribiera imagenes/pin.gif la pagina no me mostraria 
+nada porque el index.html esta en un subdirectorio diferente.
+lo guarde en el sub directorio 'web' que es desde donde abro el editor.
+Esto ../ le indica al editor que tiene que "subir" hasta el subdirectorio
+/Practica1-1 y desde alli ingresar al subdirectorio /imagenes
+Lo mismo en la seccion de html, para que encuentre el script. 
+*/
