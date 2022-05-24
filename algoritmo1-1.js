3a0b6dae3ca76542371d4ba5e55844dcd2eab18c
aeseb <sebastoledano@outlook.com>
2022-05-24 15:42:14 -0300

    script 1

--- algoritmo1-1.js
+++ algoritmo1-1.js
@@ -1,13 +1,3 @@
 
 document.write("<div align=center><h1>Mis respetos a los de Sombrero <FONT COLOR='grey'>Gris</FONT></h1></div>");
-document.write("<div align=center><img src='../imagenes/pin.gif' alt='gif'</div>");
-/*
-Los ../ se usan para navegar en sub directorios dentro del directorio
-principal.
-Si solo escribiera imagenes/pin.gif la pagina no me mostraria 
-nada porque el index.html esta en un subdirectorio diferente.
-lo guarde en el sub directorio 'web' que es desde donde abro el editor.
-Esto ../ le indica al editor que tiene que "subir" hasta el subdirectorio
-/Practica1-1 y desde alli ingresar al subdirectorio /imagenes
-Lo mismo en la seccion de html, para que encuentre el script. 
-*/
+document.write("<div align=center><img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fbf%2F4f%2F66%2Fbf4f665d2d868929847e0b6f6e638cff.gif&f=1&nofb=1'");
